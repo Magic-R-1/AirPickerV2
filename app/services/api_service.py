d877da2005fc01397af23e6046f71728d6939671
Erwan <egretillat@gmail.com>
2025-01-03 23:01:27 +0100

    Premier appel API, et imbrication des services

---
+++ app/services/api_service.py
@@ -0,0 +1,10 @@
+from nba_api.stats.static import players
+
+class ApiService:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_players(self):
+        # Utilisation de la fonction get_players de nba_api pour obtenir les joueurs
+        return players.get_players()
