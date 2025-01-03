8bb2188e8542b0125943ac636c438078d77a2e26
Erwan <egretillat@gmail.com>
2025-01-03 22:02:14 +0100

    Exemple de main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 from models.player import Player
 from models.team import Team
 
+def main():
+    # Création d'instances de la classe Player
+    player1 = Player(id=1, name="LeBron James", team="Los Angeles Lakers")
+    player2 = Player(id=2, name="Stephen Curry", team="Golden State Warriors")
+
+    # Affichage des informations sur les joueurs
+    print(player1)
+    print(player2)
+
+if __name__ == "__main__":
+    main()
