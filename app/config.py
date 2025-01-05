7cc77c15d98b008655bbda1ed315309644065e05
Erwan <egretillat@gmail.com>
2025-01-05 17:54:55 +0100

    Conf des niveaux de log

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
+import logging
 from dotenv import load_dotenv
 
 # Charger les variables du fichier .env
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-#load_dotenv()
+load_dotenv()
 
 class Config:
 
@@ -16,10 +16,10 @@ class Config:
     DB_NAME = os.getenv("DB_NAME")
     DATABASE_URL = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" # Créer l'URL de connexion à la base de données
 
-    # Configurations générales
-    DEBUG = os.getenv("DEBUG", "false").lower() in ("true", "1", "yes")
-    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+    # Configurer le logging global avec le niveau provenant du .env
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+    logging.basicConfig(level=LOG_LEVEL)
 
-    # Ajoute d'autres paramètres si nécessaire
+    # NBA API config
     NBA_API_TEMPO = int(os.getenv("NBA_API_TEMPO", 60))
     NBA_API_TEMPO_PLAYERS = int(os.getenv("NBA_API_TEMPO_PLAYERS", 100))
