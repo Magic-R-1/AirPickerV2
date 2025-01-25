5fdc2d8ab864498d7b3d792e8aab41f053d8ec5a
Erwan <egretillat@gmail.com>
2025-01-25 16:19:18 +0100

    Logs

--- app/config.py
+++ app/config.py
@@ -16,6 +16,16 @@ class Config:
     DB_NAME = os.getenv("DB_NAME")
     DATABASE_URL = f"{DB_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" # Créer l'URL de connexion à la base de données
 
+    # Récupérer le logger spécifique à SQLAlchemy
+    sqlalchemy_logger = logging.getLogger('sqlalchemy.engine')
+    # Supprimer tous les handlers existants
+    for handler in sqlalchemy_logger.handlers[:]:
+        sqlalchemy_logger.removeHandler(handler)
+    # Configurer le niveau de log de SQLAlchemy
+    sqlalchemy_logger.setLevel(logging.WARNING)
+    # Désactiver la propagation des logs SQLAlchemy
+    sqlalchemy_logger.propagate = False
+
     # Configurer le logging global avec le niveau provenant du .env
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
     logging.basicConfig(level=LOG_LEVEL)
