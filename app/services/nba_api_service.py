9619b079a65b01f6b746b33aeb1c7a38089dd1ed
Erwan <egretillat@gmail.com>
2025-01-29 22:16:18 +0100

    Simplification de NbaApiService

--- app/services/nba_api_service.py
+++ app/services/nba_api_service.py
@@ -16,67 +16,51 @@ class NbaApiService:
     # 1. Player
     # =================================
     @staticmethod
-    def get_raw_players() -> list[dict]:
-        return players.get_players()
-
-    @staticmethod
     def get_players() -> pd.DataFrame:
         """
-        Récupère les joueurs via l'API, les transforme en DataFrame et renomme les colonnes.
+        input : list[dict]
+        Transformation en DataFrame, et renommage des colonnes.
         :return: pd.DataFrame: Liste des joueurs.
         """
-
-        list_dicts = NbaApiService.get_raw_players()
+        list_dicts = players.get_players()  # Appel API
         df = pd.DataFrame(list_dicts)
         df_ameliore = Utils.ameliore_df(df)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df_ameliore, NbaApiEndpoints.PLAYERS_GET_PLAYERS.value)
         return df_renamed
 
-    @staticmethod #Done
-    def get_raw_common_player_info(player_id) -> commonplayerinfo:
-        return commonplayerinfo.CommonPlayerInfo(player_id=player_id)
-
     @staticmethod
     def get_common_player_info(player_id) -> pd.DataFrame:
         """
-        Récupère l'objet CommonPlayerInfo via l'API, le transforme en DataFrame et renomme les colonnes.
-        :return: pd.DataFrame: Informations des joueurs.
+        input : objet commonplayerinfo
+        Transformation en DataFrame, et renommage des colonnes.
+        :return: pd.DataFrame: Informations du joueur.
         """
-
-        raw_data = NbaApiService.get_raw_common_player_info(player_id)
+        raw_data = commonplayerinfo.CommonPlayerInfo(player_id=player_id)   # Appel API
         df = Utils.obtenir_df_manipulable(raw_data)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.COMMON_PLAYER_INFO.value)
         return df_renamed
 
     @staticmethod
-    def get_raw_player_game_log_by_player_id(player_id: int) -> playergamelog:
-        return playergamelog.PlayerGameLog(player_id=player_id, season=Config.SAISON_EN_COURS)
-
-    @staticmethod
     def get_player_game_log_by_player_id(player_id: int) -> pd.DataFrame:
         """
-        Récupère l'objet PlayerGameLog via l'API, le transforme en DataFrame, renomme les colonnes, et converti les dates.
+        input : objet playergamelog
+        Transformation en DataFrame, renommage des colonnes, et convertion des dates.
         :return: pd.DataFrame: Logs des boxscores du joueur.
         """
-
-        raw_data = NbaApiService.get_raw_player_game_log_by_player_id(player_id)
+        raw_data = playergamelog.PlayerGameLog(player_id=player_id, season=Config.SAISON_EN_COURS)  # Appel API
         df = Utils.obtenir_df_manipulable(raw_data)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.PLAYER_GAME_LOG.value)
         df_renamed["game_date"] = df_renamed["game_date"].apply(Utils.convert_to_date)  # Convertir les valeurs JUN 19, 2003 en Date 2003-06-19
         return df_renamed
 
     @staticmethod
-    def get_raw_player_next_games_by_player_id(player_id: int) -> playernextngames:
-        return playernextngames.PlayerNextNGames(player_id=player_id)
-
-    @staticmethod
     def get_player_next_games_by_player_id(player_id: int) -> pd.DataFrame:
         """
-        Récupère l'objet PlayerNextNGames via l'API, le transforme en DataFrame, renomme les colonnes, et converti les dates.
+        input : objet playernextngames
+        Transformation en DataFrame, renommage des colonnes, et convertion des dates.
         :return: pd.DataFrame: Logs des prochains matchs du joueur.
         """
-
-        raw_data = NbaApiService.get_raw_player_next_games_by_player_id(player_id)
+        raw_data = playernextngames.PlayerNextNGames(player_id=player_id)   # Appel API
         df = Utils.obtenir_df_manipulable(raw_data)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.PLAYER_GAME_LOG.value)
         df_renamed["game_date"] = df_renamed["game_date"].apply(Utils.convert_to_date)  # Convertir les valeurs JUN 19, 2003 en Date 2003-06-19
@@ -85,51 +69,39 @@ class NbaApiService:
     # 2. Team
     # =================================
     @staticmethod
-    def get_raw_teams() -> list[dict]:
-        return teams.get_teams()
-
-    @staticmethod
     def get_teams() -> pd.DataFrame:
         """
-        Récupère les équipes via l'API, les transforme en DataFrame et renomme les colonnes.
+        input : list[dict]
+        Transformation en DataFrame, et renommage des colonnes.
         :return: pd.DataFrame: Liste des équipes.
         """
-
-        list_dicts = NbaApiService.get_raw_teams()
+        list_dicts = teams.get_teams()  # Appel API
         df = pd.DataFrame(list_dicts)
         df_ameliore = Utils.ameliore_df(df)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df_ameliore, NbaApiEndpoints.TEAMS_GET_TEAMS.value)
         return df_renamed
 
     @staticmethod
-    def get_raw_team_roster_by_team_id(team_id: int) -> commonteamroster:
-        return commonteamroster.CommonTeamRoster(team_id=team_id)
-
-    @staticmethod
     def get_team_roster_by_team_id(team_id: int) -> commonteamroster:
         """
-        Récupère l'objet CommonTeamRoster via l'API, le transforme en DataFrame, renomme les colonnes, et converti les dates.
+        input : objet commonteamroster
+        Transformation en DataFrame, renommage des colonnes, et convertion des dates.
         :return: pd.DataFrame: Informations des rosters.
         """
-
-        raw_data = NbaApiService.get_raw_team_roster_by_team_id(team_id)
+        raw_data = commonteamroster.CommonTeamRoster(team_id=team_id)   # Appel API
         df = Utils.obtenir_df_manipulable(raw_data)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.COMMON_TEAM_ROSTER.value)
         df_renamed["birthdate"] = df_renamed["birthdate"].apply(Utils.convert_to_date)  # Convertir les valeurs JUN 19, 2003 en Date 2003-06-19
         return df_renamed
 
     @staticmethod
-    def get_raw_team_game_log_by_team_id(team_id: int) -> teamgamelog:
-        return teamgamelog.TeamGameLog(team_id=team_id, season=Config.SAISON_EN_COURS)
-
-    @staticmethod
     def get_team_game_log_by_team_id(team_id: int) -> pd.DataFrame:
         """
-        Récupère l'objet TeamGameLog via l'API, le transforme en DataFrame, renomme les colonnes, et converti les dates.
+        input : objet teamgamelog
+        Transformation en DataFrame, renommage des colonnes, et convertion des dates.
         :return: pd.DataFrame: Logs des boxscores des équipes.
         """
-
-        raw_data = NbaApiService.get_raw_team_game_log_by_team_id(team_id)
+        raw_data = teamgamelog.TeamGameLog(team_id=team_id, season=Config.SAISON_EN_COURS)  # Appel API
         df = Utils.obtenir_df_manipulable(raw_data)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.TEAM_GAME_LOG.value)
         df_renamed["game_date"] = df_renamed["game_date"].apply(Utils.convert_to_date)  # Convertir les valeurs JUN 19, 2003 en Date 2003-06-19
@@ -138,17 +110,13 @@ class NbaApiService:
     # 3. Feuilles de match
     # =================================
     @staticmethod
-    def get_raw_boxscore_by_game_id(game_id: str) -> boxscoretraditionalv3:
-        return boxscoretraditionalv3.BoxScoreTraditionalV3(game_id=game_id)
-
-    @staticmethod
     def get_boxscore_by_game_id(game_id: str) -> pd.DataFrame:
         """
-        Récupère l'objet BoxScoreTraditionalV3 via l'API, le transforme en DataFrame et renomme les colonnes.
+        input : objet boxscoretraditionalv3
+        Transformation en DataFrame, et renommage des colonnes.
         :return: pd.DataFrame: Informations des boxscores.
         """
-
-        raw_data = NbaApiService.get_raw_boxscore_by_game_id(game_id)
+        raw_data = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id=game_id) # Appel API
         df = Utils.obtenir_df_manipulable(raw_data)
         df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.BOX_SCORE_TRADITIONAL_V3.value)
         return df_renamed
