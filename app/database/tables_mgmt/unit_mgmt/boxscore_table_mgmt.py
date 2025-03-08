8e92ae2952d163e05f6216763bbe97f5be7be559
Erwan <egretillat@gmail.com>
2025-03-09 00:34:23 +0100

    Update de boxscore en base ok

--- app/database/tables_mgmt/unit_mgmt/boxscore_table_mgmt.py
+++ app/database/tables_mgmt/unit_mgmt/boxscore_table_mgmt.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from tqdm import tqdm
 
 from app.database.db_connector import engine, SessionLocal
@@ -7,7 +5,7 @@ from app.models.boxscore import Boxscore
 from app.dao.teamgamelog_dao import TeamGameLogDAO
 from app.services.boxscore_service import BoxscoreService
 from app.services.nba_api_service import NbaApiService
-from app.services.team_service import TeamService
+from app.dao.boxscore_dao import BoxscoreDAO
 
 
 class BoxscoreTableMgmt:
@@ -18,54 +16,42 @@ class BoxscoreTableMgmt:
         print("Table 'boxscore' créée avec succès.")
 
     @staticmethod
-    # TODO : reste à gérer les joueurs pas en base car inactif, mais présent dans des boxscores, comme 203926
-    # https://chatgpt.com/share/67c4c3b4-146c-8004-9dca-e77ad52aef08
     def fill_boxscore_table():
-        # Récupérer la liste des ids de toutes les équipes dans la table team
-        team_ids_list = TeamService.get_list_all_team_ids()
+        BoxscoreTableMgmt.update_boxscore_table()
 
-        # Boucler sur les joueurs pour ajouter leurs informations
+    @staticmethod
+    def update_boxscore_table():
+        liste_game_id_in_teamgamelog = set(TeamGameLogDAO.get_list_unique_game_ids())  # Convertir en set dès le début
+        liste_game_id_in_boxscore = set(BoxscoreDAO.get_list_unique_game_ids())
+
+        liste_nouveaux_game_id = liste_game_id_in_teamgamelog - liste_game_id_in_boxscore  # Set opération directe
+
         with SessionLocal() as db:
             try:
-                # Parcourir les équipes
-                for team_id in tqdm(
-                        team_ids_list,
-                        desc="Boucle sur les équipes",
-                        unit="équipe",
-                        total=len(team_ids_list)
+                # Parcourir les game_id
+                for game_id in tqdm(
+                        liste_nouveaux_game_id,
+                        desc="Boucle sur les game_id",
+                        unit="game_id",
+                        total=len(liste_nouveaux_game_id)
                 ):
 
-                    # Liste des game_id
-                    liste_game_ids = TeamGameLogDAO.get_list_game_id_by_team_id(team_id)
+                    # Obtenir le DF boxscore
+                    df_boxscore = NbaApiService.get_boxscore_by_game_id(game_id)
 
-                    # Parcourir les game_id
-                    for game_id in tqdm(
-                            liste_game_ids,
-                            desc="Boucle sur les game_id",
-                            unit="game_id",
-                            total=len(liste_game_ids)
-                    ):
+                    # Mapper les données vers le modèle Boxscore
+                    list_boxscore_sqlalchemy = BoxscoreService.map_boxscore_df_to_list_boxscore_model(df_boxscore)
 
-                        # Obtenir le DF boxscore
-                        df_boxscore = NbaApiService.get_boxscore_by_game_id(game_id)
+                    if list_boxscore_sqlalchemy:
+                        db.bulk_save_objects(list_boxscore_sqlalchemy)  # Insertion en batch
 
-                        # Mapper les données vers le modèle Boxscore
-                        list_boxscore_sqlalchemy = BoxscoreService.map_boxscore_df_to_list_boxscore_model(df_boxscore)
-
-                        if list_boxscore_sqlalchemy:
-                            db.bulk_save_objects(list_boxscore_sqlalchemy)  # Insertion en batch
-
-                db.flush()  # Flush avant commit pour s'assurer que les objets sont envoyés à la base de données
+                # Augmenter l'indentation de un si nécessaire
+                db.flush()
                 db.commit()
 
             except Exception as e:
                 db.rollback()
                 print(f"Une erreur est survenue lors de l'ajout des boxscores : {e}")
-
-    @staticmethod
-    # TODO
-    def update_boxscore_table():
-        print("")
 
     @staticmethod
     def clear_boxscore_table():
