05ac05bca1a4b375bb55853a8a3292e73ca5aa4a
Erwan <egretillat@gmail.com>
2025-03-09 15:16:06 +0100

    Gestion de l'appel à scoreboardv2

--- app/services/nba_api_service.py
+++ app/services/nba_api_service.py
@@ -5,6 +5,8 @@ from nba_api.stats.endpoints import boxscoretraditionalv3, commonplayerinfo, com
 
 from datetime import date
 
+from pandas import DataFrame
+
 from app.config.config import Config
 from app.utils.nba_api_column_mapper import NbaApiColumnMapper
 from app.enums.nba_api_endpoints import NbaApiEndpoints
@@ -18,11 +20,11 @@ class NbaApiService:
     # 1. Player
     # =================================
     @staticmethod
-    def get_players() -> pd.DataFrame:
+    def get_players() -> DataFrame:
         """
         input : list[dict]
         Transformation en DataFrame, et renommage des colonnes.
-        :return: pd.DataFrame: Liste des joueurs.
+        :return: DataFrame : Liste des joueurs.
         """
         try:
             list_dicts = players.get_players()  # Appel API
@@ -36,11 +38,11 @@ class NbaApiService:
         return df_renamed
 
     @staticmethod
-    def get_common_player_info(player_id) -> pd.DataFrame:
+    def get_common_player_info(player_id) -> DataFrame:
         """
         input : objet commonplayerinfo
         Transformation en DataFrame, et renommage des colonnes.
-        :return: pd.DataFrame: Informations du joueur.
+        :return: DataFrame : Informations du joueur.
         """
         try:
             raw_data = commonplayerinfo.CommonPlayerInfo(player_id=player_id)  # Appel API
@@ -53,11 +55,11 @@ class NbaApiService:
         return df_renamed
 
     @staticmethod
-    def get_player_game_log_by_player_id(player_id: int) -> pd.DataFrame:
+    def get_player_game_log_by_player_id(player_id: int) -> DataFrame:
         """
         input : objet playergamelog
         Transformation en DataFrame, renommage des colonnes, et convertion des dates.
-        :return: pd.DataFrame: Logs des boxscores du joueur.
+        :return: DataFrame : Logs des boxscores du joueur.
         """
         try:
             raw_data = playergamelog.PlayerGameLog(player_id=player_id, season=Config.SAISON_EN_COURS)  # Appel API
@@ -72,11 +74,11 @@ class NbaApiService:
         return df_renamed
 
     @staticmethod
-    def get_player_next_games_by_player_id(player_id: int) -> pd.DataFrame:
+    def get_player_next_games_by_player_id(player_id: int) -> DataFrame:
         """
         input : objet playernextngames
         Transformation en DataFrame, renommage des colonnes, et convertion des dates.
-        :return: pd.DataFrame: Logs des prochains matchs du joueur.
+        :return: DataFrame : Logs des prochains matchs du joueur.
         """
         try:
             raw_data = playernextngames.PlayerNextNGames(player_id=player_id)  # Appel API
@@ -92,11 +94,11 @@ class NbaApiService:
     # 2. Team
     # =================================
     @staticmethod
-    def get_teams() -> pd.DataFrame:
+    def get_teams() -> DataFrame:
         """
         input : list[dict]
         Transformation en DataFrame, et renommage des colonnes.
-        :return: pd.DataFrame: Liste des équipes.
+        :return: DataFrame : Liste des équipes.
         """
         try:
             list_dicts = teams.get_teams()  # Appel API
@@ -110,11 +112,11 @@ class NbaApiService:
         return df_renamed
 
     @staticmethod
-    def get_team_roster_by_team_id(team_id: int) -> pd.DataFrame:
+    def get_team_roster_by_team_id(team_id: int) -> DataFrame:
         """
         input : objet commonteamroster
         Transformation en DataFrame, renommage des colonnes, et convertion des dates.
-        :return: pd.DataFrame: Informations des rosters.
+        :return: DataFrame : Informations des rosters.
         """
         try:
             raw_data = commonteamroster.CommonTeamRoster(team_id=team_id)  # Appel API
@@ -128,11 +130,11 @@ class NbaApiService:
         return df_renamed
 
     @staticmethod
-    def get_team_game_log_by_team_id(team_id: int) -> pd.DataFrame:
+    def get_team_game_log_by_team_id(team_id: int) -> DataFrame:
         """
         input : objet teamgamelog
         Transformation en DataFrame, renommage des colonnes, et convertion des dates.
-        :return: pd.DataFrame: Logs des boxscores des équipes.
+        :return: DataFrame : Logs des boxscores des équipes.
         """
         try:
             raw_data = teamgamelog.TeamGameLog(team_id=team_id, season=Config.SAISON_EN_COURS)  # Appel API
@@ -148,11 +150,11 @@ class NbaApiService:
     # 3. Feuilles de match
     # =================================
     @staticmethod
-    def get_boxscore_by_game_id(game_id: str) -> pd.DataFrame:
+    def get_boxscore_by_game_id(game_id: str) -> DataFrame:
         """
         input : objet boxscoretraditionalv3
         Transformation en DataFrame, et renommage des colonnes.
-        :return: pd.DataFrame: Informations des boxscores.
+        :return: DataFrame : Informations des boxscores.
         """
         try:
             raw_data = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id=game_id)  # Appel API
@@ -165,23 +167,63 @@ class NbaApiService:
         return df_renamed
 
     @staticmethod
-    def get_scoreboardv2_by_game_date(game_date: date) -> pd.DataFrame:
+    def get_scoreboardv2_by_game_date(game_date: date) -> tuple[DataFrame, DataFrame]:
         """
         input : objet scoreboardv2
-        Transformation en DataFrame, renommage des colonnes, et convertion des dates.
-        :return: pd.DataFrame: Informations des matchs.
+        Transformation en DataFrame, renommage des colonnes, et conversion des dates.
+        :return: DataFrame : Informations des matchs.
         """
         try:
-            raw_data = scoreboardv2.ScoreboardV2(game_date=game_date)  # Appel API
+            # Appel à l'API ScoreboardV2 pour obtenir les données des matchs du jour
+            raw_data = scoreboardv2.ScoreboardV2(game_date=game_date)
         except Exception as e:
+            # En cas d'échec de l'appel API, afficher l'erreur et retourner None
             print(f"Erreur lors de l'appel à ScoreboardV2 avec game_date={game_date} : {e}")
             raw_data = None  # Valeur par défaut en cas d'échec
 
-        df = Utils.obtenir_df_manipulable(raw_data)
-        df_renamed = NbaApiColumnMapper.rename_columns_in_df(df, NbaApiEndpoints.SCORE_BOARD_V2.value)
-        df_renamed["game_date"] = df_renamed["game_date"].apply(Utils.convert_to_date)
+        # Obtention et transformation du dataframe contenant les informations des matchs
+        df_game_header = Utils.obtenir_df_manipulable(raw_data)
+        df_game_header_tmp = NbaApiColumnMapper.rename_columns_in_df(df_game_header,
+                                                                     NbaApiEndpoints.SCORE_BOARD_V2.value)
+        df_game_header_tmp["game_date"] = df_game_header_tmp["game_date"].apply(Utils.convert_to_date)
 
-        return df_renamed
+        # Sélection des colonnes nécessaires et ajout de colonnes pour les scores
+        df_game_header_final = df_game_header_tmp[
+            ["game_date", "game_id", "game_status_text", "home_team_id", "visitor_team_id"]].copy()
+        df_game_header_final['home_team_score'] = None  # Initialisation des scores à None
+        df_game_header_final['visitor_team_score'] = None  # Initialisation des scores à None
+
+        # Obtention et transformation du dataframe contenant les scores des équipes
+        df_line_score = Utils.obtenir_df_manipulable(raw_data, 1)
+        df_line_score_final = NbaApiColumnMapper.rename_columns_in_df(df_line_score,
+                                                                      NbaApiEndpoints.SCORE_BOARD_V2.value)
+        df_line_score_final["game_date"] = df_line_score_final["game_date"].apply(Utils.convert_to_date)
+
+        # Fonction pour récupérer le score d'une équipe en fonction de son team_id
+        def get_team_score(team_id, df_line_score_tmp):
+            team_row = df_line_score_tmp[
+                df_line_score_tmp['team_id'] == team_id]  # Filtre des lignes correspondant à l'équipe
+            if not team_row.empty:
+                return team_row['pts'].values[0]  # Récupère le score de l'équipe
+            return None  # Retourne None si aucune donnée n'est trouvée
+
+        # Mise à jour des scores pour chaque équipe dans df_game_header_final
+        for index, row in df_game_header_final.iterrows():
+            home_team_id = row['home_team_id']  # ID de l'équipe à domicile
+            home_team_score = get_team_score(home_team_id,
+                                             df_line_score_final)  # Récupère le score de l'équipe à domicile
+            if home_team_score is not None:
+                df_game_header_final.loc[index, 'home_team_score'] = home_team_score  # Mise à jour du score à domicile
+
+            visitor_team_id = row['visitor_team_id']  # ID de l'équipe visiteuse
+            visitor_team_score = get_team_score(visitor_team_id,
+                                                df_line_score_final)  # Récupère le score de l'équipe visiteuse
+            if visitor_team_score is not None:
+                df_game_header_final.loc[
+                    index, 'visitor_team_score'] = visitor_team_score  # Mise à jour du score visiteur
+
+        # Retourne le DataFrame avec les informations des matchs et les scores
+        return df_game_header_final
 
 
 if __name__ == "__main__":
@@ -192,7 +234,6 @@ if __name__ == "__main__":
     # data = NbaApiService.get_team_game_log_by_team_id(1610612747)
     # data = NbaApiService.get_player_game_log_by_player_id(2544)
     # data = NbaApiService.get_player_next_games_by_player_id(2544)
-    # data = NbaApiService.get_scoreboardv2_by_game_date(date(2025,1,30))
-    data = NbaApiService.get_boxscore_by_game_id("0022400001")
-
+    data = NbaApiService.get_scoreboardv2_by_game_date(date(2025, 1, 25))
+    # data = NbaApiService.get_boxscore_by_game_id("0022400001")
     print("")
